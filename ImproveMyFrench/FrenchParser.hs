855b1f6c2eb39703c9dbb9d2d10dfada61356c02
imediava <txomin_pelu@yahoo.es>
2015-05-03 23:57:30 +0200

    First version that ask for a correction of all errors

---
+++ ImproveMyFrench/FrenchParser.hs
@@ -0,0 +1,37 @@
+module ImproveMyFrench.FrenchParser where
+
+import Text.ParserCombinators.Parsec
+
+-- data ErroredWord = Alternative String String deriving (Show)
+data Word = Correct String | ErroredWord String String
+
+instance Show Word where
+    show (ErroredWord error correct)   = "[" ++ error ++ "|" ++ correct ++ "]"
+    show (Correct word) = word
+
+data Sentence = Sentence [Word] deriving (Show)
+
+sentence :: Parser Sentence
+sentence = 
+    do result <- mword
+       eof
+       return (Sentence result)
+
+-- Each cell contains 0 or more characters, which must not be a comma or
+-- EOL
+mword :: Parser [Word]
+mword = (sepBy ( choice [(try erroredWord), (fmap Correct (many1 frenchAlphaNum))] ) (many space))
+
+frenchAlphaNum :: Parser Char
+frenchAlphaNum = oneOf "'-?" <|> letter
+
+erroredWord :: Parser Word
+erroredWord = do prefix <- many frenchAlphaNum
+                 char '['
+                 w1 <- many1 frenchAlphaNum
+                 char '|'
+                 w2 <- many1 frenchAlphaNum
+                 char ']'
+                 postfix <- many frenchAlphaNum
+                 return (ErroredWord (prefix ++ w1 ++ postfix) (prefix ++ w2 ++ postfix))
+
---
+++ Main.hs
@@ -0,0 +1,45 @@
+--
+-- Copyright (c) 2006 Don Stewart - http://www.cse.unsw.edu.au/~dons/
+-- GPL version 2 or later (see http://www.gnu.org/copyleft/gpl.html)
+--
+import System.Environment
+import ImproveMyFrench.FrenchParser
+import Text.Parsec
+import Data.List.Split
+import Data.Either
+import Data.List
+ 
+
+correctWord :: Word => String
+correctWord (Correct x) = x
+correctWord (ErroredWord w c) = c
+
+wrongWord :: Word => String
+wrongWord (Correct x) = x
+wrongWord (ErroredWord w c) = w
+
+correctSentence :: Sentence => [String]
+correctSentence (Sentence s) = map correctWord s
+
+wrongSentence :: Sentence => [String]
+wrongSentence (Sentence s) = map wrongWord s
+
+askQuestion :: Sentence => IO ()
+askQuestion sentence = do
+   let wSentence = (wrongSentence sentence)
+   putStrLn $ "?> " ++ (intercalate " " wSentence)
+   putStrLn "$> "
+   input <- getLine
+   putStrLn $ show $ (splitOn " " input) == (correctSentence sentence)
+
+
+-- | 'main' runs the main program
+main :: IO ()
+main = do
+   args <- getArgs
+   content <- readFile (args !! 0) 
+   let linesOfFiles = lines content
+       parsed = map (\l -> parse sentence "" l)  linesOfFiles
+   mapM_ askQuestion (rights parsed)
+       
+ 
